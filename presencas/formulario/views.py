fb6842b2e65b865b8282c464d0f08dc39d3747d2
Thiago Castro <thiago.castro99@hotmail.com>
2024-09-06 15:27:43 -0300

    Ajuste de lógica na checagem de imagens e correção na geração de novos botões

--- presencas/formulario/views.py
+++ presencas/formulario/views.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, request, flash
 from .formulario import FormularioArtista
+from werkzeug.datastructures import ImmutableOrderedMultiDict
 
 formulario_blueprint = Blueprint("formulario", __name__)
+
 
 @formulario_blueprint.route('/formulario', methods=['GET', 'POST'])
 def index():
 
+    request.parameter_storage_class = ImmutableOrderedMultiDict
     form = FormularioArtista(request.form)
     
     if form.validate_on_submit() and request.method == "POST":
