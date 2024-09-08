5c401eaba6bb988c327569994274919cb8a6670e
Thiago Castro <thiago.castro99@hotmail.com>
2024-09-07 23:04:11 -0300

    Refatoração do template do formulário e adição de mensagens de erro

--- presencas/formulario/views.py
+++ presencas/formulario/views.py
@@ -10,6 +10,9 @@ def index():
 
     request.parameter_storage_class = ImmutableOrderedMultiDict
     form = FormularioArtista(request.form)
+    form.erros = dict()
+    form.erros['links']= [{'titulo_url' : 'Erro1', 'url' : 'Erro2'}, {'titulo_url' : 'Erro3', 'url' : 'Erro4'}]
+    form.erros['imagens']= [{'titulo' : 'Erro1', 'material' : 'Erro2'}, {'titulo' : 'Erro3', 'material' : 'Erro4'}]
     
     if form.validate_on_submit() and request.method == "POST":
         print(request.form)
