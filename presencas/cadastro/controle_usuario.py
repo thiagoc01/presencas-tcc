e08e0845636f17caf01fcbb449d1646ac34a9140
Thiago Castro <thiago.castro99@hotmail.com>
2024-11-02 20:23:27 -0300

    Implementação de alteração de senha
    Acrescenta uma opção de alteração de senha pro usuário atual apenas, e não pra outros.
    Também foi ajustada a separação das linhas de código e a macro que exibe erros pra campos
    fora de uma lista de outros campos também foi ajustada para que possam ser exibidos mais de um erro
    para um campo de entrada.

--- presencas/cadastro/controle_usuario.py
+++ presencas/cadastro/controle_usuario.py
@@ -3,8 +3,22 @@ from flask_login import current_user
 from wtforms.fields import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired
 from .usuario import Usuario
-from presencas import db
+from presencas import db, bcrypt_var
 import re
+
+def _verifica_minimalidade_senha(form):
+
+    regex_simbolos = re.compile("[@_!#$%^&*()<>?/\|}{~:\.]")
+    regex_minuscula = re.compile("[a-z]")
+    regex_maiuscula = re.compile("[A-Z]")
+
+    regex_numero = re.compile("[0-9]")
+    if re.search(regex_simbolos, form.senha.data) == None or re.search(regex_minuscula, form.senha.data) == None \
+            or re.search(regex_maiuscula, form.senha.data) == None or re.search(regex_numero, form.senha.data) == None \
+            or len(form.senha.data) < 10:
+
+        form.erros["senha"] = "A senha deve ter pelo menos um caractere especial, uma letra maiúscula e minúscula, um número e 10 caracteres ao menos"
+
 
 class FormularioCadastro(FlaskForm):
 
@@ -17,35 +31,41 @@ class FormularioCadastro(FlaskForm):
     confirmar_senha = PasswordField('', [InputRequired("Insira a senha para confirmar")], render_kw=dict(placeholder="Digite a senha novamente"))
 
     def validate(self, extra_validators):
+
         validacao_form = super(FormularioCadastro, self).validate()
+
         if not validacao_form:
+
             if self.usuario.data == "":
+
                 self.erros["usuario"] = "Digite um nome de usuário"
+
             if self.senha.data == "":
+
                 self.erros["senha"] = "Digite uma senha"
+
             if self.confirmar_senha.data == "":
+
                 self.erros["confirmar_senha"] = "Digite a confirmação da senha"
+
             return False
         usuario = db.session.execute(db.select(Usuario).filter_by(usuario = self.usuario.data)).first()
         if usuario:
+
             self.erros["usuario"] = "Usuário já existente"
 
         if self.senha.data != self.confirmar_senha.data:
+
             self.erros["confirmar_senha"] = "As senhas não coincidem"
 
-        regex_simbolos = re.compile("[@_!#$%^&*()<>?/\|}{~:\.]")
-        regex_minuscula = re.compile("[a-z]")
-        regex_maiuscula = re.compile("[A-Z]")
-        regex_numero = re.compile("[0-9]")
-        if re.search(regex_simbolos, self.senha.data) == None or re.search(regex_minuscula, self.senha.data) == None \
-                or re.search(regex_maiuscula, self.senha.data) == None or re.search(regex_numero, self.senha.data) == None \
-                or len(self.senha.data) < 10:
-            self.erros["senha"] = "A senha deve ter pelo menos um caractere especial, uma letra maiúscula e minúscula, um número e 10 caracteres ao menos"
+        _verifica_minimalidade_senha(self)
 
         if len(self.erros.items()) > 0:
+
             return False
+
         return True
-    
+
 class FormularioRemocao(FlaskForm):
 
     class Meta:
@@ -54,16 +74,80 @@ class FormularioRemocao(FlaskForm):
     usuario = StringField('', [InputRequired("Insira o usuário para cadastro")], render_kw=dict(placeholder=""))
 
     def validate(self, extra_validators):
+
         validacao_form = super(FormularioRemocao, self).validate()
+
         if not validacao_form:
+
             if self.usuario.data == "":
+
                 self.erros["usuario"] = "Digite um nome de usuário"
+
             return False
+
         if self.usuario.data == current_user.usuario:
+
             self.erros["usuario"] = "O usuário a ser deletado não pode ser igual ao atual"
+
             return False
         usuario = db.session.execute(db.select(Usuario).filter_by(usuario = self.usuario.data)).first()
         if usuario == None:
+
             self.erros["usuario"] = "Usuário não cadastrado"
+
             return False
+
+        return True
+
+class FormularioAlteracaoSenha(FlaskForm):
+
+    class Meta:
+        csrf = True
+
+    senha = PasswordField('', [InputRequired("Insira a senha para cadastro")], render_kw=dict(placeholder=""))
+    confirmar_senha = PasswordField('', [InputRequired("Insira a senha para confirmar")], render_kw=dict(placeholder="Digite a senha novamente"))
+
+    def validate(self, extra_validators):
+
+        validacao_form = super(FormularioAlteracaoSenha, self).validate()
+
+        if not validacao_form:
+
+            if self.senha.data == "":
+
+                self.erros["senha"] = "Digite uma senha"
+
+            if self.confirmar_senha.data == "":
+
+                self.erros["confirmar_senha"] = "Digite a confirmação da senha"
+
+            return False
+
+        try:
+
+            usuario = db.session.execute(db.select(Usuario).filter_by(usuario = current_user.usuario)).first()
+
+        except Exception:
+            raise
+
+        if self.senha.data != self.confirmar_senha.data:
+
+            self.erros["confirmar_senha"] = "As senhas não coincidem"
+
+        _verifica_minimalidade_senha(self)
+
+        if bcrypt_var.check_password_hash(usuario[0].senha, self.senha.data):
+
+            if self.erros.get('senha') == None:
+
+                self.erros["senha"] = "A senha nova não pode ser igual à atual"
+
+            else:
+
+                self.erros["senha"] = [self.erros.get("senha"), "A senha nova não pode ser igual à atual"]
+
+        if len(self.erros.items()) > 0:
+
+            return False
+
         return True
--- presencas/cadastro/views.py
+++ presencas/cadastro/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from presencas import bcrypt_var, db
 from .usuario import Usuario
 from .login import FormularioLogin
-from .controle_usuario import FormularioCadastro, FormularioRemocao
+from .controle_usuario import FormularioCadastro, FormularioRemocao, FormularioAlteracaoSenha
 from sqlalchemy.exc import DBAPIError, OperationalError
 
 cadastro_blueprint = Blueprint("cadastro", __name__)
@@ -33,8 +33,11 @@ def login():
 @cadastro_blueprint.route("/logout/")
 @login_required
 def logout():
+
     if current_user.is_authenticated:
+
         logout_user()
+
     return redirect(url_for("cadastro.login"))
 
 @cadastro_blueprint.route("/criar_usuario/", methods=['GET', 'POST'])
@@ -67,22 +70,59 @@ def criar_usuario():
 def remover_usuario():
 
     if not current_user.e_adm:
+
         abort(403)
 
     form = FormularioRemocao(request.form)
     form.erros = dict()
 
     if form.validate_on_submit():
+
         erro = None
-        usuario = db.session.execute(db.select(Usuario).filter_by(usuario = form.usuario.data)).first()[0]
+
         try:
+
+            usuario = db.session.execute(db.select(Usuario).filter_by(usuario = form.usuario.data)).first()[0]
             db.session.delete(usuario)
             db.session.commit()
+
         except (DBAPIError, OperationalError) as e:
+
             erro = e
+
         except Exception:
+
             abort(500)
 
         return render_template('cadastro/log_cadastro.html', usuario = form.usuario.data, erro = erro, cadastrar_usuario = False)
 
     return render_template('cadastro/cadastro_usuario.html', form = form, cadastrar_usuario = False)
+
+@cadastro_blueprint.route("/alterar_senha/", methods=['GET', 'POST'])
+@login_required
+def alterar_senha():
+
+    form = FormularioAlteracaoSenha(request.form)
+    form.erros = dict()
+
+    if form.validate_on_submit():
+
+        erro = None
+
+        try:
+
+            usuario = db.session.execute(db.select(Usuario).filter_by(usuario = current_user.usuario)).first()[0]
+            usuario.senha = bcrypt_var.generate_password_hash(form.senha.data)
+            db.session.commit()
+
+        except (DBAPIError, OperationalError) as e:
+ 
+            erro = e
+
+        except Exception:
+
+            abort(500)
+
+        return render_template('cadastro/log_alteracao_senha.html', erro = erro)
+
+    return render_template('cadastro/alteracao_senha.html', form = form)
